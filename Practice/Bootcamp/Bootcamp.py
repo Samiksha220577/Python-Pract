9372e17b2cd33afd4134105a5c97fbd624033793
Samiksha220577 <sami.codeai@gmail.com>
2024-06-17 19:44:05 +0530

    6/17/2024-1

--- Practice/Bootcamp/Bootcamp.py
+++ Practice/Bootcamp/Bootcamp.py
@@ -13,14 +13,15 @@
 # s.display()
 # --------------------------------------------
 # single inheritance
-class Parent:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
-    def add(self):
-        print(f"the addition of a,b is :")
-class child(Parent):
-    def multiply(self):
-        print(f"the multiplication of 5 , 6 is:")
-c=child(1,5)
-c.add()
+# class Parent:
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def add(self):
+#         print(f"the addition of a,b is :")
+# class child(Parent):
+#     def multiply(self):
+#         print(f"the multiplication of 5 , 6 is:")
+# c=child(1,5)
+# c.add()
+# ---------------------------------------------
